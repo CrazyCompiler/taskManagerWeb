2d4247a03303a0dabfa607fd266357fc0597b873
brindaban <brindaba@thoughtworks.com>
2016-04-29 12:05:20 +0530

    changed the name ConfigObject to context

--- app/taskManager-svc/main.go
+++ app/taskManager-svc/main.go
@@ -11,7 +11,7 @@ import (
 )
 
 func main() {
-	configObject := config.ContextObject{}
+	context := config.Context{}
 	errorLogFilePath := "errorLog"
 	errorFile, err := os.OpenFile(errorLogFilePath, os.O_APPEND|os.O_WRONLY, 0600)
 	if err != nil {
@@ -19,21 +19,21 @@ func main() {
 	}
 	defer errorFile.Close()
 
-	configObject.ErrorLogFile = errorFile
+	context.ErrorLogFile = errorFile
 
 	var serverAddressFlag = flag.String("sa","127.0.0.1:8080","listening to the service")
 	var portFlag = flag.String("p","8888","To which port it will listen")
 
 	flag.Parse()
 
-	configObject.ServerAddress = *serverAddressFlag
+	context.ServerAddress = *serverAddressFlag
 	port := *portFlag
 
 	if err != nil {
-		errorHandler.ErrorHandler(configObject.ErrorLogFile, err)
+		errorHandler.ErrorHandler(context.ErrorLogFile, err)
 	}
 
-	routers.HandleRequests(configObject)
+	routers.HandleRequests(context)
 	err = http.ListenAndServe(":"+port, nil)
 	if err != nil {
 		fmt.Println("their was error ", err)
